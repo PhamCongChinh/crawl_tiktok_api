ba933239bd0d164a03f313978d9e4c63b1459f46
DESKTOP-KQA2RP4\ThinkPad <chinhpham1609@gmail.com>
2025-10-15 17:37:30 +0100

    666

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+TOPIC = os.getenv("TOPIC")
+KAFKA_BROKER = os.getenv("KAFKA_BROKER")
